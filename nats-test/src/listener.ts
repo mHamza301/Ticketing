7b8d574398642b0e1a272dbc93add8eab37594c7
mHamza301 <mHamza301@users.noreply.github.com>
2021-02-02 00:00:18 +0500

    Testing NATS STREAMING

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,32 @@
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+console.clear();
+
+const client = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+    url: 'http:localhost:4222'
+});
+
+client.on('connect', () => {
+    console.log('Listener connected to NATS');
+
+    const options = client
+        .subscriptionOptions()
+        .setManualAckMode(true);
+
+    const subscription = client.subscribe(
+        'ticket:created', 
+        'orders-srv-queue-grp',
+        options
+    );
+
+    subscription.on('message', (msg: Message) =>{
+        const data = msg.getData();
+
+        if (typeof data === 'string') {
+            console.log(`Recieved event #${msg.getSequence()}, with data: ${data}`);
+        }
+
+        msg.ack();
+    });
+});
---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,23 @@
+import nats from 'node-nats-streaming';
+
+console.clear();
+
+const client = nats.connect('ticketing', 'abc', {
+    url: 'http://localhost:4222'
+});
+
+client.on('connect', () => {
+    console.log('Publisher connected to NATS');
+
+
+    var data = JSON.stringify({
+        id: '12abc',
+        title: 'Ticket',
+        price: 20
+    });
+
+    client.publish('ticket:created', data, () => {
+        console.log('Event Published');
+    });
+
+});
